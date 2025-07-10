5a941824c36412dafc48d9c86912656280006cb3
Juan Gómez <gomezmartinjd@gmail.com>
2025-07-10 18:16:18 +0200

    finishing mocks and spys lesson

--- Go-WithTests/mocking/main.go
+++ Go-WithTests/mocking/main.go
@@ -4,12 +4,34 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
-func Countdown(out io.Writer) {
-	fmt.Fprint(out, "3")
+const finalWord = "Go!"
+const countDownStart = 3
+
+type Sleeper interface {
+	Sleep()
+}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countDownStart; i > 0; i-- {
+		_, _ = fmt.Fprintln(out, i)
+		sleeper.Sleep()
+	}
+
+	_, _ = fmt.Fprint(out, finalWord)
 }
 
 func main() {
-	Countdown(os.Stdout)
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
+	Countdown(os.Stdout, sleeper)
 }
