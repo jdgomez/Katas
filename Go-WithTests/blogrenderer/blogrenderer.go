4803fb6e1c9797b9abc795bd6e5a82fd1657d81a
Juan Gómez <gomezmartinjd@gmail.com>
2026-01-18 14:20:34 +0100

    commiting top and bottom templates, and a refactor to use a common template renderer. Benchamrks added

--- Go-WithTests/blogrenderer/blogrenderer.go
+++ Go-WithTests/blogrenderer/blogrenderer.go
@@ -13,49 +13,29 @@ type Post struct {
 	Tags        []string
 }
 
+type PostRenderer struct {
+	templ *template.Template
+}
+
 var (
 	//go:embed "templates/*"
 	postTemplates embed.FS
 )
 
-func Render(w io.Writer, post Post) error {
-	blogTemplate, err := template.ParseFS(postTemplates, "templates/*.gohtml")
+func NewPostRenderer() (*PostRenderer, error) {
+	templ, err := template.ParseFS(postTemplates, "templates/*.gohtml")
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	if err := blogTemplate.Execute(w, post); err != nil {
+	return &PostRenderer{templ: templ}, nil
+}
+
+func (r *PostRenderer) Render(w io.Writer, post Post) error {
+
+	if err := r.templ.ExecuteTemplate(w, "blog.gohtml", post); err != nil {
 		return err
 	}
 
 	return nil
 }
-
-//func Render(w io.Writer, p Post) error {
-//	_, err := fmt.Fprintf(w, "<h1>%s</h1>", p.Title)
-//	if err != nil {
-//		return err
-//	}
-//
-//	_, err = fmt.Fprintf(w, "\n<p>%s</p>", p.Description)
-//	if err != nil {
-//		return err
-//	}
-//
-//	_, err = fmt.Fprintf(w, "\nTags: <ul>")
-//	if err != nil {
-//		return err
-//	}
-//
-//	for _, tag := range p.Tags {
-//		_, err = fmt.Fprintf(w, "<li>%s</li>", tag)
-//		if err != nil {
-//			return err
-//		}
-//	}
-//	_, err = fmt.Fprintf(w, "</ul>")
-//	if err != nil {
-//		return err
-//	}
-//	return err
-//}
--- Go-WithTests/blogrenderer/blogrenderer_test.go
+++ Go-WithTests/blogrenderer/blogrenderer_test.go
@@ -2,6 +2,7 @@ package blogrenderer_test
 
 import (
 	"bytes"
+	"io"
 	"testing"
 
 	"github.com/approvals/go-approval-tests"
@@ -18,23 +19,39 @@ func TestRender(t *testing.T) {
 		}
 	)
 
+	postRenderer, err := blogrenderer.NewPostRenderer()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	t.Run("it converts a single post into HTML", func(t *testing.T) {
 		buff := bytes.Buffer{}
-		err := blogrenderer.Render(&buff, aPost)
 
-		if err != nil {
+		if err := postRenderer.Render(&buff, aPost); err != nil {
 			t.Fatal(err)
 		}
 
 		approvals.VerifyString(t, buff.String())
-
-		//		got := buff.String()
-		//		want := `<h1>hello world</h1>
-		//<p>This is a description</p>
-		//Tags: <ul><li>go</li><li>tdd</li></ul>`
-		//
-		//		if got != want {
-		//			t.Errorf("got '%s', want '%s'", got, want)
-		//		}
 	})
+}
+
+func BenchmarkRender(b *testing.B) {
+	var (
+		aPost = blogrenderer.Post{
+			Title:       "hello world",
+			Body:        "This is a post",
+			Description: "This is a description",
+			Tags:        []string{"go", "tdd"},
+		}
+	)
+	postRenderer, err := blogrenderer.NewPostRenderer()
+
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	for b.Loop() {
+		postRenderer.Render(io.Discard, aPost)
+	}
 }
